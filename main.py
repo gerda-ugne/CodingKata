a7f68da8689b17f427450f7429494bc20fa01fb5
gerda-ugne <55397925+gerda-ugne@users.noreply.github.com>
2021-04-09 12:06:39 +0100

    Committed initial files

--- main.py
+++ main.py
@@ -3,46 +3,114 @@ from random import randint
 
 class Character:
 
-    def __init__(self, weapon=None, health=1000, level=1, alive=True, stamina=100):
+    def __init__(self, id, health=1000, level=1, alive=True, stamina=100, mana=20):
         self.health = health
         self.level = level
         self.alive = alive
         self.experience = 0
         self.stamina = stamina
-        self.weapon = weapon
+        self.id = id
+        self.factions = []
+        self.mana = mana
+        self.weapon = None
 
-    def deal_damage(self, character):
+######################################
+    def join_faction(self, faction):
+        if faction not in self.factions:
+            self.factions.append(faction)
 
-        if self == character:
+    def leave_faction(self, faction):
+        if faction in self.factions:
+            self.factions.remove(faction)
+
+    def is_ally(self, c):
+        return len(set(self.factions).intersection(c.factions)) != 0
+
+    def dead(self):
+        if self.health == 0:
+            self.alive = False
+
+    def heal(self, c):
+        self.dead()
+        if ((self.id == c.id) or self.is_ally(c)) and self.alive and self.health < 1000 and self.mana >= 10:
+            self.health += 500
+            self.mana -= 10
+            if self.health > 1000:
+                self.health = 1000
+            return True
+        return False
+
+    def heal_self(self):
+        return self.heal(self)
+#############################################
+
+    def deal_damage(self, enemy):
+        if self == enemy:
             return False
         if self.stamina < 10:
             return False
+        if not enemy.alive:
+            return False
 
-        damage = randint(1, 100)
+        #damage = randint(1, 100)
+        damage = 50
+        if self.weapon is not None:
+            damage += self.weapon.attack_value
+
         self.stamina -= 10
-
-        character.health -= damage
-        if character.health <= 0:
-            character.health = 0
-            character.alive = False
+        enemy.health -= damage
+        if enemy.health <= 0:
+            enemy.health = 0
+            enemy.alive = False
 
         return True
 
     def add_experience(self, experience_points):
-        requirements = pow(2, self.level) * 10
-        self.experience += experience_points
-        if self.experience == requirements:
+        requirements = pow(self.level, 2) * 10
+        """We have the requirements.
+        First see how many points we need to the next level."""
+        points_to_next_level = requirements - self.experience
+
+        if experience_points >= points_to_next_level:
+            experience_points -= points_to_next_level
+        else:
+            self.experience += experience_points
+            return
+
+        while experience_points >= 0:
+            self.experience += points_to_next_level
             self.experience = 0
-            self.level = self.level + 1
-        elif self.experience > requirements:
-            experience_bank = self.experience - requirements
-            self.experience = experience_bank
-            self.level = self.level + 1
+            self.level += 1
+
+            requirements = pow(self.level, 2) * 10
+            points_to_next_level = requirements - self.experience
+            if experience_points >= points_to_next_level:
+                experience_points -= points_to_next_level
+            else:
+                self.experience += experience_points
+                return
 
     def increment_stamina(self):
         self.stamina += 5
         if self.stamina > 100:
             self.stamina = 100
+
+    def equip(self, weapon):
+        if weapon.level_req <= self.level:
+
+            if self.weapon:
+                self.weapon.equipped = False
+                self.weapon = None
+
+            self.weapon = weapon
+            weapon.equipped = True
+            return True
+        else:
+            return False
+
+    def unequip(self, weapon):
+        weapon.equipped = False
+        player.weapon = None
 
 
 class Weapon:
@@ -51,21 +119,11 @@ class Weapon:
         self.level_req = level_req
         self.equipped = equipped
 
-    def equip(self):
-        self.equipped = True
-
-    def unequip(self):
-        self.equipped = False
-
 
 if __name__ == '__main__':
-    player = Character()
-    enemy = Character()
+    player = Character(1)
+    enemy = Character(2)
 
-    player.deal_damage(enemy)
-    print("Health of Enemy:" + str(enemy.health))
-    player.add_experience(20)
-    print("Exp of player:" + str(player.experience))
-    print("Level of player:" + str(player.level))
+    player.add_experience(200)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- test.py
+++ test.py
@@ -1,15 +1,160 @@
 import unittest
-from main import Character
+from main import Character, Weapon
 
 
 class Testing(unittest.TestCase):
 
-   def test_deal_damage(self):
-       player = Character()
-       enemy = Character()
-       result = player.deal_damage(enemy)
+    def test_deal_damage(self):
+        player = Character(1)
+        enemy = Character(2)
+
+        original_health = enemy.health
+        player.deal_damage(enemy)
+        assert original_health - 50 == enemy.health
+        assert enemy.health > 0
+
+    def test_deal_damage_with_weapon(self):
+        player = Character(1)
+        player.equip(Weapon(20, 0))
+        enemy = Character(2)
+
+        original_health = enemy.health
+        player.deal_damage(enemy)
+        assert original_health - 70 == enemy.health
+        assert enemy.health > 0
+
+    def test_deal_too_much_damage(self):
+        player = Character(1)
+        enemy = Character(2)
+
+        enemy.health = 20
+        player.deal_damage(enemy)
+        assert enemy.health == 0
+        assert enemy.alive == False
+
+    def test_self_damage(self):
+        player = Character(1)
+        original_health = player.health
+        player.deal_damage(player)
+        assert original_health == player.health
+
+    def test_low_stamina_damage(self):
+        player = Character(1)
+        enemy = Character(2)
+
+        player.stamina = 2
+        original_health = enemy.health
+        player.deal_damage(enemy)
+        assert original_health == enemy.health
+
+    def test_dead_enemy_damage(self):
+        player = Character(1)
+        enemy = Character(2)
+
+        enemy.alive = False
+        original_health = enemy.health
+        player.deal_damage(enemy)
+        assert original_health == enemy.health
+
+    def test_increment_stamina(self):
+        player = Character(1)
+        player.stamina = 20
+        player.increment_stamina()
+        assert player.stamina == 25
+
+    def test_increment_excessive_stamina(self):
+        player = Character(1)
+        player.increment_stamina()
+        assert player.stamina == 100
 
     def test_add_experience_less_than_one_level(self):
-        player = Character()
+        player = Character(1)
         player.add_experience(6)
-        self.assertEqual(player.experience,6)
+        self.assertEqual(player.experience, 6)
+
+    def test_decrement_stamina_when_attacking(self):
+        player = Character(1)
+        enemy = Character(2)
+
+        original_stamina = player.stamina
+        player.deal_damage(enemy)
+        assert original_stamina - 10 == player.stamina
+
+    def test_add_experience_of_one_level(self):
+        player = Character(1)
+        player.add_experience(10)
+        self.assertEqual(player.experience, 0)
+        self.assertEqual(player.level, 2)
+
+    def test_add_more_experience_than_a_level(self):
+        player = Character(1)
+        player.add_experience(12)
+        self.assertEqual(player.experience, 2)
+        self.assertEqual(player.level, 2)
+
+    def test_equip_weapon(self):
+        player = Character(1)
+        weapon = Weapon(0, 0)
+        player.equip(weapon)
+
+        assert player.weapon == weapon
+        assert weapon.equipped == True
+
+    def test_equip_too_high_level_weapon(self):
+        player = Character(1)
+        weapon = Weapon(0, 20)
+        player.equip(weapon)
+
+        assert player.weapon == None
+        assert weapon.equipped == False
+
+    #############################################
+    def test_init_health(self):
+        character = Character(1)
+        assert character.health == 1000
+
+    def test_init_level(self):
+        character = Character(1)
+        assert character.level == 1
+
+    def test_init_alive(self):
+        character = Character(1)
+        assert character.alive
+
+    def test_init_factions(self):
+        character = Character(1)
+        assert not character.factions
+
+    def test_init_mana(self):
+        character = Character(1)
+        assert character.mana == 20
+
+    def test_heal_self1(self):
+        character = Character(1)
+        character.heal_self()
+        assert character.health == 1000
+
+    def test_heal_self2(self):
+        character = Character(1)
+        character.heal_self()
+        character.health = 100
+        print(character.heal_self())
+        assert character.health == 600
+
+    def test_heal_self3(self):
+        character = Character(1)
+        character.health = 100
+        character.heal_self()
+        character.heal_self()
+        assert character.health == 1000
+
+    def test_heal(self):
+        char1 = Character(1)
+        char2 = Character(2)
+        factions = ['f1', 'f2', 'f3', 'f4', 'f5']
+        for x in factions[0:3]:
+            char1.join_faction(x)
+        for x in factions[2:5]:
+            char2.join_faction(x)
+        char1.health = 300
+        assert char1.heal(char2)
